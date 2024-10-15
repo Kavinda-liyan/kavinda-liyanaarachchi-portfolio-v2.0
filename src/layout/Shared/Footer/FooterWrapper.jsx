6d3ff06f7ddc9662e93a959408c69bbc1e1d407d
Kavinda-liyan <110520736+Kavinda-liyan@users.noreply.github.com>
2024-10-15 09:55:09 +0530

    footerwrapper

--- src/layout/Shared/Footer/FooterWrapper.jsx
+++ src/layout/Shared/Footer/FooterWrapper.jsx
@@ -2,7 +2,7 @@ const FooterWrapper =({children,Title})=>{
     return(
 
         <>
-        <div className='flex flex-col text-center border-r-1 border-slate-300 max-md:border-r-0 max-md:border-b-1 max-md:py-1'>
+        <div className='flex flex-col text-center border-r-1 border-slate-300 max-md:border-r-0  max-md:py-1'>
             <h1 className='font-ubuntu text-xs font-semibold text-slate-600'>{Title}</h1>
                 <div className="flex flex-nowrap justify-center h-full">
                     {children}
