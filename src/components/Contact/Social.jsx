bf2bd1592d1e36c0a0cb0eed895761a6893ac0d5
Kavinda-liyan <110520736+Kavinda-liyan@users.noreply.github.com>
2024-10-23 11:21:24 +0530

    image replacement public folder

--- src/components/Contact/Social.jsx
+++ src/components/Contact/Social.jsx
@@ -1,6 +1,7 @@
-import { faFacebook, faGithub, faInstagram, faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faGithub, faInstagram, faLinkedin, faPinterest, faWhatsapp, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Socialbtn from "./Socialbtn";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 
 const Social = ()=>{
@@ -8,14 +9,16 @@ const Social = ()=>{
         <>
         <div className="bg-slate-300 flex justify-center"> 
            
-                        <ul className="flex flex-row items-center justify-center">
+                        <div className="flex items-center justify-center ">
                            
                            <Socialbtn link="https://www.linkedin.com/in/kavinda-liyanaarachchi-729b14262/" socialIcon={faLinkedin}/>
                            <Socialbtn link="https://github.com/Kavinda-liyan" socialIcon={faGithub}/>
                            <Socialbtn link="https://wa.me/94774421707" socialIcon={faWhatsapp}/>
+                           <Socialbtn link="https://x.com/cham_kaviz" socialIcon={faXTwitter} />
                            <Socialbtn link="https://www.facebook.com/kavinda.chamara.14" socialIcon={faFacebook} />
                            <Socialbtn link="https://www.instagram.com/cham_kaviz/" socialIcon={faInstagram} />
-                        </ul>
+                           <Socialbtn link="https://www.pinterest.com/KavindaLiya/" socialIcon={faPinterest} />
+                        </div>
                     </div>
         </>
     );
--- src/components/Contact/Socialbtn.jsx
+++ src/components/Contact/Socialbtn.jsx
@@ -5,11 +5,11 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 const Socialbtn = (props) => {
   return (
     <>
-    <li>
-    <a className={`flex justify-center items-center scale-75 m-1  font-bold border-2 border-red-500 
-     hover:border-red-600 rounded-full p-0 w-24 h-24 max-sm:w-8 max-sm:h-8  text-6xl max-md:text-sm hover:shadow-sm
-      hover:shadow-red-100 hover:text-red-600 hover:scale-90 transition  duration-500 text-red-500`} target='_blank' href={props.link}><FontAwesomeIcon icon={props.socialIcon}/></a>
-    </li>
+    
+    <a className={`flex justify-center items-center scale-50 m-1  font-bold border-2 border-red-500 
+     hover:border-red-600 rounded-full px-5 w-24 h-24 max-sm:w-8 max-sm:h-8  text-6xl max-md:text-sm hover:shadow-sm
+      hover:shadow-red-100 hover:text-red-600 hover:scale-75 transition  duration-500 text-red-500`} target='_blank' href={props.link}><FontAwesomeIcon icon={props.socialIcon}/></a>
+    
    
                         
     </>
